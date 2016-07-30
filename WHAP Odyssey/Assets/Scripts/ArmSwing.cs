c51ed5b9fe04458a942523199089d56f579e8514
ProjectMousa <projectmousa@gmail.com>
2016-07-29 22:55:55 -0400

    Door Toggle Added
    Door Toggle Added

---
+++ WHAP Odyssey/Assets/Scripts/ArmSwing.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmSwing : MonoBehaviour
+{
+    bool Swing = false;
+    public Animator m_Anim;
+    Collider2D Colliders;
+    public float blockTimer;
+    public bool Sword = false;
+    public bool Axe = false;
+
+    // Use this for initialization
+    void Awake()
+    {
+        m_Anim = GetComponent<Animator>();
+        Colliders = GetComponent<Collider2D>();
+    }
+
+    void Start()
+    {
+        Colliders.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (blockTimer > 0)
+        {
+            blockTimer -= Time.unscaledDeltaTime;
+        }     
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {            
+                    Swing = true;
+                    m_Anim.SetBool("Swing", true);
+                    Colliders.enabled = true;                            
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            Swing = false;
+            m_Anim.SetBool("Swing", false);
+            Colliders.enabled = false;
+            m_Anim.SetBool("Sword", false);
+            m_Anim.SetBool("Axe", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            if (GameObject.Find("KP").GetComponent<PlayerControl>().InventoryOpen == false) {
+                if (GameObject.Find("KP").GetComponent<PlayerControl>().GameMenuOpen == false) {
+                    if (GameObject.Find("KP").GetComponent<PlayerControl>().BuyMenuOpen == false)
+                    {
+                        if (blockTimer <= 0)
+                        {
+                            SwordBlock();                           
+                        }
+                    }
+                }
+            }                             
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            m_Anim.SetBool("Block", false);          
+        }
+    }
+    void SwordBlock() {
+        m_Anim.SetBool("Block", true);
+    }
+}
---
+++ WHAP Odyssey/Assets/Scripts/KP_HouseDoorCollider.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KP_HouseDoorCollider : MonoBehaviour {
+
+    public GameObject player;
+    Collider2D Colliders;
+    public bool DoorOpen;
+
+    // Use this for initialization
+    void Start () {
+        DoorOpen = false;
+        player = GameObject.Find("KP");
+        Colliders.enabled = true;
+        GameObject.Find("KP_HouseOutside").GetComponent<KP_House>().m_Anim.SetBool("DoorOpen", false);     
+    }
+    void Awake() {
+        Colliders = GetComponent<Collider2D>();
+    }    
+
+    // Update is called once per frame
+    void Update () {
+    float PlayerNear = Vector3.Distance(player.transform.position, transform.position);
+      if (Input.GetKeyDown(KeyCode.E)) {           
+        if (PlayerNear <= 0.2) {
+                if (DoorOpen == true)
+                {
+                    GameObject.Find("KP_HouseOutside").GetComponent<KP_House>().m_Anim.SetBool("DoorOpen", false);
+                    Colliders.enabled = true;
+                    DoorOpen = false;
+                }
+                else if (DoorOpen == false) {
+                    GameObject.Find("KP_HouseOutside").GetComponent<KP_House>().m_Anim.SetBool("DoorOpen", true);
+                    Colliders.enabled = false;
+                    DoorOpen = true;
+                }       
+            }
+     }
+   }   
+}
